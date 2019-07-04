26c5b65af76d3c552c553aaa5e28efb8cd5229d0
Salt-Shaker <20408920+salt-shaker@users.noreply.github.com>
2019-07-04 13:06:35 -0700

    Update

---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = transform.position;
+        position.x = position.x + 0.1f;
+        transform.position = position;
+    }
+}
