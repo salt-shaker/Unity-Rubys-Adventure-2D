431ae85093f059e00464ad770e9bee2173a7ead6
Salt-Shaker <20408920+salt-shaker@users.noreply.github.com>
2019-07-06 17:24:20 -0700

    35 Percent Complete
    35 percent complete with Unity tutorial, Rubys Adventure: 2D.

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -13,8 +13,13 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float speedBoost = 3.0f;
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + speedBoost * horizontal * Time.deltaTime;
+        position.y = position.y + speedBoost * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
